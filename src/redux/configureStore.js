c4e3f569a6fbafd44af9b4c8b10022583dfdf819
Edward <oluyaratosin123@gmail.com>
2022-02-11 07:37:23 +0100

    Add navigation to my companyInc app

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,11 +3,14 @@ import {
 } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import countriesReducer from './Home/home';
+import companyReducer from './Company/company';
 
 const middlewareList = [thunk, logger];
 
 const reducer = combineReducers({
-  // reducers
+  countriesReducer,
+  companyReducer,
 });
 
 const store = createStore(
